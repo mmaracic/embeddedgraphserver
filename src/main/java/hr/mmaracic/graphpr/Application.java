d246aa4d283bb3e09f9ce5c8368f725e2e84ee02
mmaracic <marijo.maracic@gmail.com>
2022-06-27 00:49:05 +0200

    Paths fix to focus target folder

--- src/main/java/hr/mmaracic/graphpr/Application.java
+++ src/main/java/hr/mmaracic/graphpr/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.io.File;
+import java.nio.file.Path;
 import java.time.Duration;
 
 import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAME;
@@ -35,7 +35,7 @@ public class Application {
 
     @Bean
     public DatabaseManagementService getDatabaseManagementService() {
-        DatabaseManagementService managementService = new DatabaseManagementServiceBuilder(new File("graphdb"))
+        DatabaseManagementService managementService = new DatabaseManagementServiceBuilder(Path.of("graphdb"))
                 .setConfig(GraphDatabaseSettings.pagecache_memory, "512M")
                 .setConfig(GraphDatabaseSettings.transaction_timeout, Duration.ofSeconds(60))
                 .setConfig(GraphDatabaseSettings.preallocate_logical_logs, true).build();
